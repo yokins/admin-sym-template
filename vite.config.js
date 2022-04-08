237fa22e7f55bb84123a152225cda3e124dd8364
syj <yokins@shi.local>
2022-04-08 18:05:16 +0800

    配置vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import compress from "vite-plugin-compress";
 import content from "@originjs/vite-plugin-content";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
-
+// import ViteRestart from "vite-plugin-restart"; 暂未使用
 
 export default defineConfig({
     resolve: {
@@ -20,9 +21,40 @@ export default defineConfig({
             runtimeOnly: false,
             include: path.resolve(__dirname, "src/i18n/locales/**")
         }),
-            legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
+        legacy({
+            targets: ["ie >= 11"],
+            additionalLegacyPolyfills: ["regenerator-runtime/runtime"]
+        }),
+        compress(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false
+            },
+            optipng: {
+                optimizationLevel: 7
+            },
+            mozjpeg: {
+                quality: 20
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: "removeViewBox"
+                    },
+                    {
+                        name: "removeEmptyAttrs",
+                        active: false
+                    }
+                ]
+            }
+        })
+        // ViteRestart({
+        //     restart: ["vite.config.[jt]s"]
+        // })
     ]
 });
