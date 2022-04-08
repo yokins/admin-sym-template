f8e5c4866668e065292e8e8d5eb31d00f3ae2863
syj <yokins@shi.local>
2022-04-08 22:28:09 +0800

    开始项目布局

---
+++ src/store/modules/global.js
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+import logo from "@/assets/images/logo.jpeg";
+
+export const useGlobalStore = defineStore("global", {
+    state: () => ({}),
+    actions: {},
+    persist: true
+});
+3;
