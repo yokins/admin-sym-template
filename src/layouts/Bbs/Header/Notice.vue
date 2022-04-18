1f6ab6b2cf827e367758a4fa7a22ef8b222878c3
syj <yokins@shi.local>
2022-04-18 15:21:08 +0800

    添加通知模块

--- src/layouts/Bbs/Header/Notice.vue
+++ src/layouts/Bbs/Header/Notice.vue
@@ -1,15 +1,10 @@
 JavaScript
 <template>
-    <n-popover :overlap="false" placement="bottom-end" trigger="click">
-        <template #trigger>
-            <div class="layouts_bbs_header_notice">
-                <n-icon size="20">
-                    <NotificationsOutline></NotificationsOutline>
-                </n-icon>
-            </div>
-        </template>
-        <div class="large-text">啊！</div>
-    </n-popover>
+    <div class="layouts_bbs_header_notice">
+        <n-icon size="20">
+            <NotificationsOutline></NotificationsOutline>
+        </n-icon>
+    </div>
 </template>
 
 <script>
